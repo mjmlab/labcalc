85b269bde76af2b8d15dc3b1e9f7cf882fc18dc2
Mark Mandel <mandel01@gmail.com>
2017-02-01 16:06:10 -0600

    Add tests for multiple gibson inserts

--- labcalc/tests/test_functions.py
+++ labcalc/tests/test_functions.py
@@ -5,5 +5,14 @@ from labcalc import gibson
 # labcalc.gibson
 
 def test_gibson_one_insert():
-    d = {'insert1': [300, 50], 'vector': [5000, 50]}
-    assert gibson.gibson_calc(d) == {'insert1': 0.24, 'vector': 2.0}
+    d = {'vector': [5000, 50], 'insert1': [300, 50]}
+    assert gibson.gibson_calc(d) == {'vector': 2.0, 'insert1': 0.24}
+
+def test_gibson_two_inserts():
+    d = {'vector': [5000, 50], 'insert1': [300, 50], 'insert2': [600, 50]}
+    assert gibson.gibson_calc(d) == {'vector': 2.0, 'insert1': 0.24, 'insert2': 0.48}
+
+def test_gibson_four_inserts():
+    d = {'vector': [5000, 50],
+         'insert1': [300, 50], 'insert2': [600, 50], 'insert3': [300, 50], 'insert4': [600, 50]}
+    assert gibson.gibson_calc(d) == {'vector': 2.0, 'insert1': 0.12, 'insert2': 0.24, 'insert3': 0.12, 'insert4': 0.24}
