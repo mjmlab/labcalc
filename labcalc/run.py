662d5f0cc21573ecf32633266ff1ca98ab771287
Mark Mandel <mandel01@gmail.com>
2017-02-01 19:47:43 -0600

    connect command line entry to run program

--- labcalc/run.py
+++ labcalc/run.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
 
 """Parse the command line arguments and call the program."""
-import click
-import logging
+from .gibson import gibson
 
-
-def parse_args(args):
-    """Parse command line arguments."""
-    #TODO(`click` logic goes here)
-    pass
-
-
-def main(args):
-    args = parse_args(args)
-
-
-if __name__ == '__main__':
-    main()
+def runner(args):
+    """Run the specified program"""
+    eval(args[0] + '()')
